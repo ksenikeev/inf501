7da76562d554befab2d026b73d4b97e23590790a
ksenikeev <ksenikeev@mail.ru>
2026-04-08 12:41:31 +0300

    Object IO

---
+++ lab2_11_data_io_stream/src/main/java/ru/itis/inf501/lab2_11/objectio/MainSerializeBuffer.java
@@ -0,0 +1,37 @@
+package ru.itis.inf501.lab2_11.objectio;
+
+import java.io.*;
+
+public class MainSerializeBuffer {
+    public static void main(String[] args) {
+        // 1. Создаем объект
+        Car car = new Car("KIA RIO", "седан", "12312313");
+        // 2. Сериализуем объект
+
+        byte[] buffer;
+
+        try (ByteArrayOutputStream bos = new ByteArrayOutputStream();
+                ObjectOutputStream os = new ObjectOutputStream(bos)) {
+
+            os.writeObject(car);
+            buffer = bos.toByteArray();
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        try (ObjectInputStream is = new ObjectInputStream(
+                new ByteArrayInputStream(buffer))) {
+            Car car1 = (Car) is.readObject();
+            System.out.println(car1);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+}
